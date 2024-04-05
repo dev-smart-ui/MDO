ba666d79d9d713c1ffa26ccbbd545edf05da6331
KatherineBond <katherine.bond74@gmail.com>
2024-04-05 14:22:22 +0300

    fixes

--- src/js/script.js
+++ src/js/script.js
@@ -17,6 +17,7 @@ window.addEventListener("load", function () {
         allowTouchMove: isTouchEnabled(),
         simulateTouch: !isTouchEnabled(),
         slidesPerView: 1,
+        speed: 800,
         longSwipesRatio: 0.3,
         mousewheel: {
             forceToAxis: true,
@@ -40,8 +41,12 @@ window.addEventListener("load", function () {
         loop: true,
         centeredSlides: true,
         roundLengths: true,
-        autoplay: true,
+        loopAdditionalSlides: 2,
+        autoplay: {
+            delay: 5000
+        },
         speed: 700,
+        watchSlidesProgress: true,
         pagination: {
             el: ".comprehensive-coverage-pagination",
             clickable: true,
@@ -50,17 +55,65 @@ window.addEventListener("load", function () {
             768: {
                 slidesPerView: 2.3,
             },
-            1200: {
+            1024: {
                 slidesPerView: 2.9,
             },
             1440: {
-                slidesPerView: 3.8,
+                slidesPerView: 3.75,
             },
             1642: {
-                slidesPerView: 4.8,
+                slidesPerView: 4.75,
+            },
+            2300: {
+                slidesPerView: 5.75,
+            }
+        },
+        on: {
+            init: function () {
+                updateSlideClasses.call(this);
+            },
+            slideChange: function () {
+                updateSlideClasses.call(this);
             }
         }
     });
+
+    function nextAll(elem) {
+        let matched = [];
+        while (elem = elem.nextSibling) {
+            if (elem.nodeType === 1) {
+                matched.push(elem);
+            }
+        }
+        return matched;
+    }
+
+    function prevAll(element) {
+        let matched = [];
+        while (element = element.previousElementSibling)
+            matched.push(element);
+        return matched;
+    }
+
+    function updateSlideClasses() {
+        let active = this.slides[this.activeIndex],
+            right = 0.18,
+            allNext = nextAll(active),
+            allPrev = prevAll(active);
+
+        active.classList.remove('afterActive');
+        active.style.right = '0'
+        this.slides.forEach(i => {
+            i.classList.remove('afterActive')
+        })
+        allPrev.forEach((elem, index) => {
+            elem.style.right = `${elem.offsetWidth * (-right * index)}px`
+        })
+        allNext.forEach((elem, index) => {
+            elem.classList.add('afterActive')
+            elem.style.right = `${elem.offsetWidth * (right * index)}px`
+        })
+    }
 
     pageSlider.on('slideChange', function () {
         const counter = this.slides[this.activeIndex].querySelector('.counter-grid');
@@ -77,7 +130,7 @@ window.addEventListener("load", function () {
         sliderTextToggle();
     })
 
-
+    //text on slider cards
     if (document.fonts) {
         document.fonts.onloadingdone = () => {
             sliderTextToggle();
@@ -86,28 +139,20 @@ window.addEventListener("load", function () {
 
     function sliderTextToggle() {
         const sliderContent = s3.querySelectorAll('.content');
-        sliderContent.forEach(i => {
-            if (i.querySelector('ul').offsetHeight > 0) {
-                if (i.closest('.swiper-slide').classList.contains('swiper-slide-active')) {
-                    i.style.transform = `translateY(0)`;
+        if(sliderContent.length > 0){
+            sliderContent.forEach(i => {
+                if (i.querySelector('ul').offsetHeight > 0) {
+                    if (i.closest('.swiper-slide').classList.contains('swiper-slide-active')) {
+                        i.style.transform = `translateY(0)`;
+                    } else {
+                        i.style.transform = `translateY(${(i.querySelector('ul').offsetHeight) + 5}px)`;
+                    }
                 } else {
-                    i.style.transform = `translateY(${(i.querySelector('ul').offsetHeight) + 3}px)`;
+                    i.style.transform = `translateY(0)`;
                 }
-            }
-        })
-    }
+            })
+        }
 
-    function sliderTextHide() {
-        const sliderContent = s3.querySelectorAll('.content ul');
-        sliderContent.forEach(element => {
-            if (element.offsetHeight > 0) {
-                element.closest('.content').style.transform = `translateY(${element.offsetHeight}px)`;
-            }
-        })
-    }
-
-    function sliderTextShow(element) {
-        element.style.transform = `translateY(0)`;
     }
 
     // counter
@@ -183,6 +228,9 @@ window.addEventListener("load", function () {
     }
 
     window.addEventListener('resize', function () {
+
+        sliderTextToggle();
+
         let numbers = document.querySelectorAll('.counter-block .number');
         numbers.forEach(element => {
             if (element.querySelectorAll('span').length > 0) {
