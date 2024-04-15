d7ae4d67a770a4c0089bc407596fe2f905d7518f
Shangri <shangrialena@gmail.com>
2024-04-15 15:33:15 +0200

    added custom package logic

--- src/js/step-form.js
+++ src/js/step-form.js
@@ -1,122 +1,18 @@
-const dataDropdownsResearchPackage = [
-    {
-        btnTitle: "Mine / Project Overview",
-        imgLink: 'src/images/step-form/listItem1.png',
-        itemsArr: ["Geographic Location", "Mine Office Address", "Development Stage", "Mine Type", "Commodities", "Life of Mine"]
-    },
-    {
-        btnTitle: "Ownership",
-        imgLink: 'src/images/step-form/listItem2.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Deposit Type & Geology",
-        imgLink: 'src/images/step-form/listItem3.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Reserves & Resources",
-        imgLink: 'src/images/step-form/listItem4.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Commodity Production",
-        imgLink: 'src/images/step-form/listItem5.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Workforce",
-        imgLink: 'src/images/step-form/listItem6.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Mine Financials",
-        imgLink: 'src/images/step-form/listItem7.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-];
+import {
+    createDropdownsOfPackageResearch,
+    createDropdownsOfPackageCustom,
+    createDropdownsOfUltimateCustom,
+    dataDropdownsResearchPackage,
+    dataDropdownsCustomPackage,
+    dataDropdownsUltimatePackage, selectedItems
+} from "/src/js/content.js";
 
-const dataDropdownsCustomPackage = [
-    {
-        btnTitle: "1Mine / Project Overview",
-        imgLink: 'src/images/step-form/listItem1.png',
-        itemsArr: ["Geographic Location", "Mine Office Address", "Development Stage", "Mine Type", "Commodities", "Life of Mine"]
-    },
-    {
-        btnTitle: "1Ownership",
-        imgLink: 'src/images/step-form/listItem2.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Deposit Type & Geology",
-        imgLink: 'src/images/step-form/listItem3.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Reserves & Resources",
-        imgLink: 'src/images/step-form/listItem4.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Commodity Production",
-        imgLink: 'src/images/step-form/listItem5.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Workforce",
-        imgLink: 'src/images/step-form/listItem6.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Mine Financials",
-        imgLink: 'src/images/step-form/listItem7.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-];
-
-const dataDropdownsUltimatePackage = [
-    {
-        btnTitle: "1Mine / Project Overview",
-        imgLink: 'src/images/step-form/listItem1.png',
-        itemsArr: ["Geographic Location", "Mine Office Address", "Development Stage", "Mine Type", "Commodities", "Life of Mine"]
-    },
-    {
-        btnTitle: "1Ownership",
-        imgLink: 'src/images/step-form/listItem2.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Deposit Type & Geology",
-        imgLink: 'src/images/step-form/listItem3.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Reserves & Resources",
-        imgLink: 'src/images/step-form/listItem4.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Commodity Production",
-        imgLink: 'src/images/step-form/listItem5.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Workforce",
-        imgLink: 'src/images/step-form/listItem6.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-    {
-        btnTitle: "Mine Financials",
-        imgLink: 'src/images/step-form/listItem7.png',
-        itemsArr: ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum"]
-    },
-];
 
 
 (() => {
     document.addEventListener('DOMContentLoaded', () => {
         let currentStep = 0;
-        const steps = document.querySelectorAll('.step');
+        const steps = document.querySelectorAll('[data-step-form]');
         const nextButtons = document.querySelectorAll('[data-next-btn]');
         const optionsDetails = document.getElementById('optionsDetails');
         const additionalTextOptionsSelect = document.getElementById('additionalTextOptionsSelect');
@@ -130,7 +26,10 @@ const dataDropdownsUltimatePackage = [
         let formData = {};
         const mainRegionSelectValue = "Global";
         let regionsIng = [];
-        let researchPackageTotal="0"
+        let researchPackageTotal=5600;
+        let ultimatePackageTotal=5600;
+        let customPackageTotal=2000;
+        let currentPackageInnerHtmRight='';
 
 
         //region custom select code start
@@ -230,99 +129,19 @@ const dataDropdownsUltimatePackage = [
 
         //right side
 
-        function createDropdownsOfPackageAndUltimateResearch(data) {
-            optionsDetails.innerHTML = '';
-            data.map((itemDropDown, index) => {
-                const dropdownBox = document.createElement("div");
-                dropdownBox.classList.add("dropdown-box");
-                index === 0 && dropdownBox.classList.add("drop-down-item-open");
-
-                const dropdownButton = document.createElement("button");
-                dropdownButton.classList.add("dropdown-toggle", "flex", "items-center", "flex-row-reverse", "justify-end");
-
-                const titleArrowBox = document.createElement("div");
-                titleArrowBox.classList.add("title-arrow-box");
-                const titleBox = document.createElement("span");
-                titleBox.innerText = itemDropDown.btnTitle;
-
-
-                titleArrowBox.appendChild(titleBox);
-                const imgBox = document.createElement("div");
-                imgBox.classList.add("dropdown-toggle-img-box", "flex", "justify-center", "items-center",);
-                const imgItem = document.createElement("img");
-                imgItem.setAttribute('src', itemDropDown.imgLink);
-                imgBox.appendChild(imgItem);
-
-                dropdownButton.appendChild(titleArrowBox);
-                dropdownButton.appendChild(imgBox);
-                dropdownBox.appendChild(dropdownButton);
-
-                const list = document.createElement("ul");
-                list.classList.add("dropdown-menu", "list-disc");
-                itemDropDown.itemsArr.map((dropDownItemInList) => {
-                    const listItem = document.createElement("li");
-                    listItem.classList.add("dropdown-item");
-                    listItem.innerText = `${dropDownItemInList}`;
-                    list.appendChild(listItem);
-                });
-
-                dropdownBox.appendChild(list);
-                optionsDetails.appendChild(dropdownBox);
-                totalCounter.innerText=researchPackageTotal;
-            });
-        }
-
-        function createDropdownsOfPackageCustom(data) {
-
-        }
-
-        function createDropdownsOfUltimateCustom(data) {
-            optionsDetails.innerHTML = '';
-            data.map((itemDropDown, index) => {
-                const menuBox = document.createElement("div");
-                menuBox.classList.add("menu-box");
-
-                const titleBox = document.createElement("div");
-                titleBox.classList.add("menu-title", "flex", "items-center", "flex-row-reverse", "justify-end");
-                titleBox.innerText = itemDropDown.btnTitle;
-
-                const imgBox = document.createElement("div");
-                imgBox.classList.add("menu-img-box", "flex", "justify-center", "items-center",);
-                const imgItem = document.createElement("img");
-                imgItem.setAttribute('src', itemDropDown.imgLink);
-                imgBox.appendChild(imgItem);
-
-
-                titleBox.appendChild(imgBox);
-                menuBox.appendChild(titleBox);
-
-                const list = document.createElement("ul");
-                list.classList.add("list-menu", "list-disc");
-                itemDropDown.itemsArr.map((dropDownItemInList) => {
-                    const listItem = document.createElement("li");
-                    listItem.classList.add("list-item");
-                    listItem.innerText = `${dropDownItemInList}`;
-                    list.appendChild(listItem);
-                });
-
-                menuBox.appendChild(list);
-                optionsDetails.appendChild(menuBox);
-                totalCounter.innerText=researchPackageTotal;
-            });
-        }
 
 
         const optionalSelectContent = {
             researchPackage: {
-                innerContent: () => createDropdownsOfPackageAndUltimateResearch(dataDropdownsResearchPackage),
+                innerContent: () => createDropdownsOfPackageResearch(dataDropdownsResearchPackage, researchPackageTotal),
                 additionalTextBottom: "Perfect for mining industry research! Encompasses Mine Type, Location, Address, Ownership, Deposit, Reserves, Commodity Production, LOM, Workforce, and Financials. Exceptonal value for your dollar!",
             },
             customPackage: {
-                innerContent: () => createDropdownsOfPackageCustom(dataDropdownsCustomPackage),
+                innerContent: () => createDropdownsOfPackageCustom(dataDropdownsCustomPackage, customPackageTotal),
                 additionalTextBottom: "Ideal for business development, specialized research or when your budget is limited! Includes all Research Package data points plus your selection of optional data modules.",
             },
             ultimatePackage: {
-                innerContent: () => createDropdownsOfUltimateCustom(dataDropdownsUltimatePackage),
+                innerContent: () => createDropdownsOfUltimateCustom(dataDropdownsUltimatePackage, ultimatePackageTotal),
                 additionalTextBottom: "Comprehensive mining intelligence! Best suitable for large corporations, consulting firms and institutional investors. Includes all Research Package data points and all optional data modules.",
             },
         };
@@ -368,21 +187,29 @@ const dataDropdownsUltimatePackage = [
                     currentStep++;
                     steps[currentStep].classList.add('active');
                     if (currentStep === 1) {
+
                         // Update formData with choices from step 1
                         formData = {
-                            regions: regionsIng,
-                            selectedOption: optionsSelect.getValue(true),
-                            optionsDetails: optionsDetails.innerHTML,
+                            selectedRegions: regionsIng,
+                            selectedPackageOption: optionsSelect.getValue(true),
+                            totalPrice:document.getElementById("totalCounter").innerText
                         };
-                        selectedOptions.innerHTML = formData.optionsDetails;
+
+                        if (optionsSelect.getValue(true) === "customPackage") {
+                            formData.selectedCustomPackageValues = selectedItems;
+                        }
+
+                        currentPackageInnerHtmRight=optionsDetails.innerHTML;
+                        selectedOptions.innerHTML = currentPackageInnerHtmRight;
                     }
                     if (currentStep === 2) {
+
                         // Final step, gather all data
                         const name = document.getElementById('name').value;
                         const email = document.getElementById('email').value;
                         const paymentMethod = document.querySelector('input[name="payment"]:checked').value;
                         Object.assign(formData, {name, email, paymentMethod});
-                        console.log(formData); // Here you can send formData to the server
+                        console.log(formData);
                     }
                 } else {
                     // Reset form and formData for demonstration purposes
@@ -390,13 +217,16 @@ const dataDropdownsUltimatePackage = [
                     regionsIng = [];
                     document.getElementById('name').value = '';
                     document.getElementById('email').value = '';
-                    document.querySelector('input[name="payment"]:checked').checked = false;
+                    document.querySelectorAll('input[name="payment"]:checked').checked = false;
                     optionsDetails.innerHTML = '';
                     selectedOptions.innerHTML = '';
                     formData = {};
                     steps[currentStep].classList.remove('active');
                     currentStep = 0;
                     steps[currentStep].classList.add('active');
+
+                    //load research package default at start
+                    optionalSelectContent.researchPackage.innerContent(dataDropdownsCustomPackage);
                 }
             });
         });
@@ -414,7 +244,7 @@ const dataDropdownsUltimatePackage = [
         });
 
         //load research package default at start
-        optionalSelectContent.researchPackage.innerContent(dataDropdownsResearchPackage);
+        optionalSelectContent.customPackage.innerContent(dataDropdownsCustomPackage);
     });
 })();
 
